e441be01edcd3e7e3aeea7a4a2f5472487e3a420
LucasBrunelli <brunelli_lucas@hotmail.com>
2024-05-03 23:04:16 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,211 @@
+import React, { useRef, useState, useEffect ,useMemo} from 'react';
+import { 
+    View, 
+    Button,
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    ScrollView,
+    TouchableOpacity,
+    FlatList,
+    PermissionsAndroid, Platform
+} from 'react-native';
+import useBLEManager from './src/APIs/BLE_Control';
+
+
+
+
+const App: React.FC = () => {  // Componete Principal
+
+
+    // Variaveis e funçoes importadas da API Bluetooth src/APIs/BLE_Control.tsx
+    const {
+        DevIdConnected,
+        DevNameConect,
+        ListDevsON,
+        SpeedNotify,
+        PowerNotify,
+        BatteryNotify,
+        CellsVoltsNotify,
+        BtnScanning,
+        requestBluetoothAndLocationPermission,
+        BleScan,
+        BleConnect,
+        BleSend,
+    } = useBLEManager();
+    
+    // Variavel para envio de dados
+    const [BleMsgToSend, setBleMsgToSend] = useState("");
+
+
+
+    // Renderização inicial do componete chamando a função de permissoes
+    useEffect(() => {
+        requestBluetoothAndLocationPermission();
+    }, []);
+    
+
+
+    // Front End Principal, aqui será chamado todas as paginas e componentes graficos
+    const scrollViewRef = useRef<ScrollView>(null);
+    return (
+
+        <ScrollView ref={scrollViewRef} style={{flex: 1}} scrollEnabled={true} keyboardShouldPersistTaps="always">
+        <View style={styles.MainView}>
+
+
+            <Button title={BtnScanning ? 'Scanning...' : 'Scan Devices'} onPress={BleScan} disabled={BtnScanning} />
+
+
+            <View 
+            style={{height: 120, width: '90%', marginTop: 20, backgroundColor: '#FFF', borderWidth: 1, borderColor: 'gray', borderRadius: 20}}>
+            <ScrollView style={{flex:1, margin:7}} scrollEnabled={true} nestedScrollEnabled={true}>
+                <FlatList
+                style={{alignSelf: 'center'}}
+                scrollEnabled={false}
+                data={ListDevsON}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <TouchableOpacity style={styles.listItem} onPress={()=> BleConnect(item)}>
+                        <Text>{item.name || 'Unnamed Device'}</Text>
+                        <Text>{item.id}</Text>
+                    </TouchableOpacity>
+                )}
+                />
+            </ScrollView>
+            </View>
+
+
+
+            <TextInputOptimized value={SpeedNotify} />
+
+            <TextInputOptimized value={PowerNotify} />
+
+            <TextInputOptimized value={BatteryNotify} />
+
+            <TextInputOptimized value={DevNameConect + "    id: " + DevIdConnected} />   
+
+
+
+            <View style={{height: 50, width: '90%', marginTop: 20, marginBottom:20, flexDirection: 'row'}}>  
+
+                <TextBoxOptimized initialValue={BleMsgToSend} setValue={setBleMsgToSend} />
+                
+                <OptimizedButton onPress={() => BleSend(BleMsgToSend)} />
+
+            </View>
+
+
+        </View>
+        </ScrollView>       
+    );
+};
+
+
+
+// Estilização simples, o ideal é que seja feita em outro arquivo
+const styles = StyleSheet.create({
+
+    MainView: {
+        marginTop:20,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    listItem: {
+        padding: 10,
+        marginVertical: 5,
+        backgroundColor: '#ddd',
+        borderColor: '#bbb',
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    
+});
+
+
+
+// Criando um componete de Visualização de texto de forma otimizada usando React.memo
+interface TextBoxOptimizedProps {
+    initialValue: string;
+    setValue: (text: string) => void; // Função para atualizar o texto externamente
+}
+const TextBoxOptimized: React.FC<TextBoxOptimizedProps> = ({ initialValue, setValue }) => {
+    const [text, setText] = useState(initialValue);  // Armazenando o texto atual
+    const scrollViewRef = useRef<ScrollView>(null);  // Ref para o ScrollView
+
+    const handleChangeText = (newText: string) => {
+        setText(newText);        // Atualiza o estado interno
+        setValue(newText); // Atualiza o valor externo
+    };
+
+    const memoizedTextInput = useMemo(() => (
+        <View style={{height: '100%', width: '80%', backgroundColor: '#FFF', borderWidth: 1, 
+            borderColor: 'gray', borderRadius: 20}}>
+            <ScrollView ref={scrollViewRef} style={{flex: 1, margin: 7}} scrollEnabled={true} nestedScrollEnabled={true}>
+                <TextInput
+                    style={{flex: 1, padding: 10, textAlignVertical: 'top', backgroundColor: '#FFF'}}
+                    multiline={true}
+                    editable={true}
+                    scrollEnabled={true}
+                    onChangeText={handleChangeText}  // Usa a função para atualizar o texto
+                    value={text}  // Exibe o texto
+                    onFocus={() => scrollViewRef.current?.scrollToEnd()}  // Rolando para o final ao ganhar foco
+                />
+            </ScrollView>        
+        </View>
+    ), [text]); // Recria o componente TextInput somente se 'text' mudar
+
+    return memoizedTextInput;
+};
+
+
+
+// Criando um componete de Visualização de texto de forma otimizada usando React.memo
+interface TextInputOptimizedProps {
+    value: string;
+}
+const TextInputOptimized: React.FC<TextInputOptimizedProps> = ({ value }) => {
+    const memoizedTextInput = useMemo(() => (
+        <View style={{height: 50, width: '90%', marginTop: 20, backgroundColor: '#FFF', borderWidth: 1, borderColor: 'gray', borderRadius: 20}}>
+            <ScrollView style={{flex: 1, margin: 7}} scrollEnabled={true} nestedScrollEnabled={true}>
+                <TextInput
+                    style={{flex: 1, padding: 10, textAlignVertical: 'top', backgroundColor: '#FFF', color: '#000'}}
+                    editable={false}
+                    multiline={true}
+                    value={value}
+                    placeholder="Data received"
+                />
+            </ScrollView>
+        </View>
+    ), [value]); // Dependendo apenas de 'value', re-renderiza somente se 'value' mudar
+
+    return memoizedTextInput;
+};
+
+
+
+// Criando um componete de Botão de forma otimizada usando React.memo
+interface OptimizedButtonProps {
+    onPress: () => void;
+}
+const OptimizedButton: React.FC<OptimizedButtonProps> = React.memo(({ onPress }) => {
+    return (
+        <TouchableOpacity 
+            style={{flex: 1, marginLeft: 10, borderRadius: 10}} 
+            onPress={onPress}>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
+                          backgroundColor: '#24b3e1', borderRadius: 10}}>
+                <Text style={{fontSize: 15, fontWeight: 'bold', color: '#000'}}>Send</Text>
+            </View>
+        </TouchableOpacity>
+    );
+});
+
+
+
+export default App;
+
+
+
---
+++ src/APIs/BLE_Control.tsx
@@ -0,0 +1,198 @@
+import React, { useState, useEffect ,useMemo } from 'react';
+import { BleManager, Device, Characteristic, Subscription } from 'react-native-ble-plx';
+import base64 from 'react-native-base64';
+import { PermissionsAndroid, Platform } from 'react-native';
+const managerBLE = new BleManager();
+
+
+
+const useBLEManager = () => {
+
+    
+    const [DevIdConnected, set_DevIdConnected] = useState('');      // ID do dispositivo conectado ex: 5C:46:B0:72:2B:6F (string)
+    const [DevNameConect, set_DevNameConect] = useState('');        // Nome do dispositivo conectado ex: Primax Bike (string)
+    const [ListDevsON, set_ListDevsON] = useState<Device[]>([]);    // Lista de dispositivos disponiveis (Device)
+    const [SpeedNotify, set_SpeedNotify] = useState('');            // Velocidade recebida de forma assincrona 
+    const [PowerNotify, set_PowerNotify] = useState('');            // Potencia recebida de forma assincrona 
+    const [BatteryNotify, set_BatteryNotify] = useState('');        // Nivel da bateria recebida de forma assincrona 
+    const [CellsVoltsNotify, set_CellsVoltsNotify] = useState('');  // Tensão por celula recebida de forma assincrona    
+    const [BtnScanning, setBtnScanning] = useState(false);          // Flag para sinalizar poresso de escaneamento    
+
+  
+
+    // Função para pedir permissoes para o aplicativo utilizar a localização e bluetooth
+    // Logica para IOS ainda deve ser definida, dentro dessa função
+    async function requestBluetoothAndLocationPermission() {
+        if (Platform.OS === 'android') {
+            const bluetoothPermissions = [
+                // PermissionsAndroid.PERMISSIONS.BLUETOOTH,
+                // PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADMIN,
+                PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+                PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+            ];
+            const locationPermission = PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION;
+
+            const bluetoothStatus = await PermissionsAndroid.requestMultiple(bluetoothPermissions);
+            const locationStatus = await PermissionsAndroid.request(locationPermission);
+    
+            if (locationStatus === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('Location permission granted');
+            } else {
+                console.log('Location permission denied');
+            }
+    
+            if (Object.values(bluetoothStatus).every(status => status === PermissionsAndroid.RESULTS.GRANTED)) {
+                console.log('Bluetooth permissions granted');
+            } else {
+                console.log('Bluetooth permissions denied');
+                return;
+            }
+        }
+    }
+
+
+
+    // Função que escanea todos dispositivos bluetooth disponiveis e coloca em uma lista do tipo Device
+    const BleScan = () => {
+    
+        setBtnScanning(true);
+        managerBLE.startDeviceScan(null, null, (error, scannedDevice) => {
+            if (error) {
+                console.error('Scan error:', error);
+                setBtnScanning(false);
+                return;
+            }
+    
+            if (scannedDevice) {
+                set_ListDevsON(prevDevices => {
+                const deviceIndex = prevDevices.findIndex(device => device.id === scannedDevice.id);
+                if (deviceIndex === -1) {
+                    return [...prevDevices, scannedDevice];
+                }
+                return prevDevices;
+                });
+            }
+        });
+    
+        // Tempo maximo de 30 segundo para escanemento
+        setTimeout(() => {
+            managerBLE.stopDeviceScan();
+            setBtnScanning(false);
+        }, 30000);
+    };
+
+
+    
+    // Função que estabelece a conexão com um dispostivio escolhido
+    async function BleConnect(dev: Device) {
+        console.log("Tentando conectar com id = " + dev.id);
+        if (DevIdConnected == '') {
+            try {
+                const device = await managerBLE.connectToDevice(dev.id);   
+                await device.discoverAllServicesAndCharacteristics();
+                set_DevIdConnected(dev.id);
+                set_DevNameConect(dev.name || '');            
+
+                // Monitoramento de Notificação Nivel da bateria recebia pela Bike
+                device.monitorCharacteristicForService(      
+                    "0000180f-0000-1000-8000-00805f9b34fb",  // UUID do serviço
+                    "00002a19-0000-1000-8000-00805f9b34fb",  // UUID da característica para notificações de Nivel da bateria
+                    (error, characteristic) => {
+                        if (error) {
+                            console.error('Erro ao monitorar característica:', error);
+                            return;
+                        }
+                        if (characteristic?.value) {
+                            const text = base64.decode(characteristic.value);
+                            set_BatteryNotify(text);
+                        }
+                    }
+                );
+
+                // Monitoramento de Notificação Potencia recebia pela Bike
+                device.monitorCharacteristicForService(      
+                    "0000180f-0000-1000-8000-00805f9b34fb",  // UUID do serviço
+                    "00002A37-0000-1000-8000-00805f9b34fb",  // UUID da característica para notificações de Potencia
+                    (error, characteristic) => {
+                        if (error) {
+                            console.error('Erro ao monitorar característica:', error);
+                            return;
+                        }
+                        if (characteristic?.value) {
+                            const text = base64.decode(characteristic.value);
+                            set_PowerNotify(text);
+                        }
+                    }
+                );
+
+                // Monitoramento de Notificação Velocidade recebia pela Bike
+                device.monitorCharacteristicForService(      
+                    "0000180f-0000-1000-8000-00805f9b34fb",  // UUID do serviço
+                    "00002A67-0000-1000-8000-00805f9b34fb",  // UUID da característica para notificações de Velocidade
+                    (error, characteristic) => {
+                        if (error) {
+                            console.error('Erro ao monitorar característica:', error);
+                            return;
+                        }
+                        if (characteristic?.value) {
+                            const text = base64.decode(characteristic.value);
+                            set_SpeedNotify(text);
+                        }
+                    }
+                );
+                
+                console.log("Dispositivo " + DevNameConect + " id: " + DevIdConnected + " Conectado!");
+
+            } catch (error) {
+                console.error('Erro ao conectar ao dispositivo:', error);
+                throw error;
+            }
+    
+        } else {
+            console.log("Dispositivo" + DevNameConect + " já esta conectado");
+        }
+    }
+    
+
+
+    // Função para enviar dados para bicicleta.
+    // Devido a uma limitação de hardware, só é possivel, enviar uma string de no maixmo 20 caracteres.
+    async function BleSend(dataSend: string) {
+        try {
+            const characteristic = await managerBLE.writeCharacteristicWithoutResponseForDevice(
+                DevIdConnected,
+                "0000180f-0000-1000-8000-00805f9b34fb",
+                "00002a19-0000-1000-8000-00805f9b34fb",
+                base64.encode(dataSend),
+            );
+            console.log('Data sent and characteristic value:', base64.decode(characteristic.value ?? ''));
+        } catch (error) {
+            console.error('Failed to send data:', error);
+        }
+    }
+
+
+
+    // Exporta Dados e Funçoes para o componente principal
+    return {
+        DevIdConnected,
+        DevNameConect,
+        ListDevsON,
+        SpeedNotify,
+        PowerNotify,
+        BatteryNotify,
+        CellsVoltsNotify,
+        BtnScanning,
+        requestBluetoothAndLocationPermission,
+        BleScan,
+        BleConnect,
+        BleSend,
+    };
+};
+
+// Exporta o componente useBLEManager
+export default useBLEManager;
+
+
+
+
